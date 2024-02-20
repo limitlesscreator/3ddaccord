9e346601a686d4e1fa2321cf16e3368837559096
limitlesscreator <0428vlad0428@mail.ru>
2024-02-20 16:53:22 +0300

    code

--- src/App.js
+++ src/App.js
@@ -6,7 +6,39 @@ function App() {
   return (
    <div className={s.main}>
 
+        <div className={s.secondHeader}>
+            <nav  className="navbar navbar-expand-lg"  style={{backgroundColor: '#0e1129'}}>
+                <div className="container-fluid">
+                    <img src={logo} alt="Logo"
+                         className="d-inline-block align-text-top"/>
+                    <button  className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+                            aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"  style={{filter: 'invert(100%) sepia(0%) saturate(7500%) hue-rotate(260deg) brightness(110%) contrast(104%)'}}></span>
+                    </button>
+                    <div className="collapse navbar-collapse"  style={{backgroundColor: '#0e1129'}}  id="navbarNav">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <a className="nav-link active text-light" aria-current="page" href="#">Home</a>
+                            </li>
+                             <li className="nav-item">
+                                <a className="nav-link active text-light" aria-current="page" href="#">Download</a>
+                            </li>
+                             <li className="nav-item">
+                                <a className="nav-link active text-light" aria-current="page" href="#">App</a>
+                            </li>
+                             <li className="nav-item">
+                                <a className="nav-link active text-light" aria-current="page" href="#">Login</a>
+                            </li>
+                             <li className="nav-item">
+                                <a className="nav-link active text-light" aria-current="page" href="#">Get Started</a>
+                            </li>
 
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
        <header>
            <img src={logo} alt="logo"/>
            <a href="#">Home</a>
